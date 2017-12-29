5621d3ad146d6bd72cbe67ba0eac4a78edcc06cc
mufufuh <mufufuh@gmail.com>
2017-12-29 12:35:30 +0700

    First commit for this project

---
+++ FXPrototype/FXPrototype/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FXPrototype.Startup))]
+namespace FXPrototype
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
